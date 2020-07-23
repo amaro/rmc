6a960c1825c675abeb77d9eb4593d377f7496290
amaro <amaro@users.noreply.github.com>
2020-07-23 16:37:09 -0700

    only use try/catch in client for arg parsing

--- client.cpp
+++ client.cpp
@@ -12,28 +12,30 @@ int main(int argc, char* argv[])
         ("h,help", "Print usage")
     ;
 
+    std::string server;
+    std::string port;
+    RMCClient client;
+
     try {
-        RMCClient client;
         auto result = opts.parse(argc, argv);
 
         if (result.count("help"))
             die(opts.help());
 
-        std::string server = result["server"].as<std::string>();
-        std::string port = result["port"].as<std::string>();
-
-        client.connect(server, port);
-
-        const char *prog = R"(void hello() { printf("hello world\n"); })";
-
-        RMC rmc(prog);
-        RMCId id = client.get_rmc_id(rmc);
-        std::cout << "got id=" << id << "\n";
-        client.call_rmc(id);
-        client.last_cmd();
-
+        server = result["server"].as<std::string>();
+        port = result["port"].as<std::string>();
     } catch (const std::exception &e) {
         std::cerr << e.what() << "\n";
         die(opts.help());
     }
+
+    client.connect(server, port);
+
+    const char *prog = R"(void hello() { printf("hello world\n"); })";
+
+    RMC rmc(prog);
+    RMCId id = client.get_rmc_id(rmc);
+    std::cout << "got id=" << id << "\n";
+    client.call_rmc(id);
+    client.last_cmd();
 }
