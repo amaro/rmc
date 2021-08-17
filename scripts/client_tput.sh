efbc6c41f9743c9c018cf705d57ecab1a299ad7f
amaro <amaro@users.noreply.github.com>
2021-08-17 15:12:16 -0700

    tput scripts

---
+++ scripts/client_tput.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "Requires 2 parameters; first is nicserver IP, second is output file"
+	exit 2
+fi
+
+ip=$1
+output=$2
+
+sudo sh -c "echo -1 > /proc/sys/kernel/sched_rt_runtime_us"
+
+for numnodes in 1 8
+do
+	for rep in {1..5}
+	do
+		echo "numnodes=${numnodes} rep=${rep}" | tee -a ${output}
+		sudo MLX5_SINGLE_THREADED=1 taskset -c 7 chrt -f 99 ./client -s ${ip} \
+			-o out --mode maxinflight | tee -a ${output}
+		sleep 15
+	done
+done
---
+++ scripts/nicserver_tput_sweephop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -x
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [[ $# -ne 1 ]]; then
+    echo "Requires numqps"
+	exit 2
+fi
+
+numqps=$1
+
+cmd() {
+    sudo MLX5_SCATTER_TO_CQE=1 MLX5_SINGLE_THREADED=1 MLX5_POST_SEND_PREFER_BF=1 \
+       	MLX_MR_ALLOC_TYPE=CONTIG MLX_MR_MIN_LOG2_CONTIG_BSIZE=16 \
+        MLX_QP_ALLOC_TYPE=HUGE MLX_CQ_ALLOC_TYPE=HUGE \
+        taskset -c 7 chrt -f 99 ./nicserver -s 10.10.1.1 -n $1 -q $2
+    sleep 10
+}
+
+for numnodes in 1 8
+do
+	for rep in {1..5}
+	do
+		cmd ${numnodes} ${numqps}
+	done
+done
