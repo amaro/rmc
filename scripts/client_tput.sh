6bd3348fba0ba5a214cce7dec24c95912c8dfa11
amaro <amaro@users.noreply.github.com>
2021-09-01 17:29:09 -0700

    scripts aware of multicore, reenable shuffle for read

--- scripts/client_tput.sh
+++ scripts/client_tput.sh
@@ -16,12 +16,25 @@ output=$3
 
 sudo sh -c "echo -1 > /proc/sys/kernel/sched_rt_runtime_us"
 
+if [[ ${threads} = "1" ]]; then
+    cpus="7"
+elif [[ ${threads} = "2" ]]; then
+    cpus="6,7"
+elif [[ ${threads} = "3" ]]; then
+    cpus="5,6,7"
+elif [[ ${threads} = "4" ]]; then
+    cpus="4,5,6,7"
+else
+    echo "threads can only equal 1, 2, 3, 4"
+    exit 2
+fi
+
 for numnodes in 1 2 4 8
 do
 	for rep in {1..5}
 	do
 		echo "numnodes=${numnodes} rep=${rep}" | tee -a ${output}
-		sudo MLX5_SINGLE_THREADED=1 taskset -c 6,7 chrt -f 99 ./client -s ${ip} \
+		sudo MLX5_SINGLE_THREADED=1 taskset -c ${cpus} chrt -f 99 ./client -s ${ip} \
 			--mode maxinflight --param ${numnodes} -t ${threads} | tee -a ${output}
 		sleep 20
 	done
--- scripts/nicserver_tput_sweephop.sh
+++ scripts/nicserver_tput_sweephop.sh
@@ -2,24 +2,37 @@
 
 set -x
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
 if [[ $# -ne 3 ]]; then
     echo "Requires numqps, workload, threads"
-	exit 2
+    exit 2
 fi
 
 numqps=$1
 workload=$2
 threads=$3
 
+if [[ ${threads} = "1" ]]; then
+    cpus="7"
+elif [[ ${threads} = "2" ]]; then
+    cpus="6,7"
+elif [[ ${threads} = "3" ]]; then
+    cpus="5,6,7"
+elif [[ ${threads} = "4" ]]; then
+    cpus="4,5,6,7"
+else
+    echo "threads can only equal 1, 2, 3, 4"
+    exit 2
+fi
+
 cmd() {
     sudo MLX5_SCATTER_TO_CQE=1 MLX5_SINGLE_THREADED=1 \
        	MLX_MR_ALLOC_TYPE=CONTIG MLX_MR_MIN_LOG2_CONTIG_BSIZE=16 \
-        taskset -c 7 chrt -f 99 ./nicserver -s 10.10.1.1 -q $1 -w $2 -t $3
+        taskset -c ${cpus} chrt -f 99 ./nicserver -s 10.10.1.1 -q $1 -w $2 -t $3
     sleep 10
 }
 
